0ffba09a3987b4c8b174a1903545e6068d99c84d
Roxedus <me@roxedus.dev>
2020-05-20 16:30:28 +0200

    wad
    wd

--- .github/FUCKING_RUN.bat
+++ .github/FUCKING_RUN.bat
@@ -1,4 +1,4 @@
 set METEOR_PROFILE=100
 set METEOR_DEBUG_BUILD=1
 set NODE_OPTIONS=--max-old-space-size=6040
-meteor build --debug --server localhost:8265 --directory /tdarr-windows-2019 --architecture os.windows.x86_64
+meteor build --debug --server localhost:8265 --directory ${{ HOME }}/tdarr-windows-2019 --architecture os.windows.x86_64
